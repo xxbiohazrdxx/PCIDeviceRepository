a89cafd20e0f0cf568da66c06b6998376ba42cce
xxbiohazrdxx <xxbiohazrdxx@users.noreply.github.com>
2024-08-20 09:46:46 -0400

    Fix null warning in processor function

--- RepositoryProcessor/Program.cs
+++ RepositoryProcessor/Program.cs
@@ -44,7 +44,7 @@ var host = new HostBuilder()
 
 		logging.Services.Configure<LoggerFilterOptions>(options =>
 		{
-			LoggerFilterRule defaultRule = options.Rules.FirstOrDefault(rule => rule.ProviderName
+			LoggerFilterRule? defaultRule = options.Rules.FirstOrDefault(rule => rule.ProviderName
 				== "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
 			if (defaultRule is not null)
 			{
